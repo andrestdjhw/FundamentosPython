8e72e316eb360ad1a842f186e9e575e0df15249f
Daniel Trujillo <guerrerohtrujillo@gmail.com>
2023-09-04 15:53:23 -0600

    Tarea Completada Variables

--- variables.py
+++ variables.py
@@ -1,1 +1,16 @@
-
+""" Hay tres tipos numéricos distintos: integers, 
+    floating point numbers y complex numbers. 
+    Además, los booleanos son un subtipo de los enteros.
+    Los enteros tiene precisión ilimitada. 
+"""
+entero = 5
+flotante = 2.5
+cadena = "Hola"
+booleano = True
+#Ejercicio numero I
+contactenado = print(cadena, entero, flotante, booleano)
+#Ejercicio numero II sumatoria de pares hasta 50
+contador = 0
+while contador <= 50:
+    print("Vuelta N: ", contador+2)
+    contador+=2
